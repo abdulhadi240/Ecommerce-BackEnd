dc758834453569f38da4e0329dda54d55ce3cb7c
abdulhadi240 <ah912425@gmail.com>
2023-04-30 16:58:22 +0500

    inital commit

---
+++ config/admin.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
+    },
+  },
+});
+
+
