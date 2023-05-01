90706f8bf52572ac99525cdd943e291f162114fc
abdulhadi240 <ah912425@gmail.com>
2023-05-01 09:36:32 +0500

    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,16 @@
 module.exports = ({ env }) => ({
     // ...
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_https://api.vercel.com/v1/integrations/deploy/prj_mAnS0ljyZoFvEFK51fc4afpzi9Qh/EQaX3laUam",
+        apiToken: "sjdZ853KONxygm1xRC4tSp1U",
+        appFilter: "ecommerce-backend",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+
     upload: {
       config: {
         provider: 'cloudinary',
@@ -16,3 +27,5 @@ module.exports = ({ env }) => ({
     },
     // ...
   });
+
+
